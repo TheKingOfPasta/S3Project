9377d0278ec65f4aa107b39805fbd0f74d9baf33
augustin.hubert <augustin.hubert@epita.fr>
2023-10-15 01:43:07 +0200

    added folder for solver

---
+++ solver/solver.h
@@ -0,0 +1,15 @@
+#pragma once
+
+
+
+void SLV_collapse_grid(short grid[9][9], int x, int y, short val);
+short SLV_get_collapsed_value(short cell_val);
+void printUnsolved(short x);
+short SLV_rmv_possibility(short* cell_val, short possibility);
+void SLV_collapse_cell(short grid[9][9],int x,int y,short val);
+void SLV_Clues_Collapsing(short grid[9][9]);
+int SLV_is_Placeable(short g[9][9], int x, int y, short val);
+void SLV_backtracking(short grid[9][9]);
+void SLV_findRemainingCells(short g[9][9],short a[]);
+void SLV_sort(short a[]);
+void SLV_solve(short grid[9][9]);
