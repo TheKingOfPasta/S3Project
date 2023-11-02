12e3d2e27d4dddac1275e0f58a79bf839bcac062
Taliayaya <ilanvinord@gmail.com>
2023-11-02 13:54:44 +0100

    removed trashfiles

--- neuron_network/pythonshell.nix
+++
@@ -1,9 +0,0 @@
-# shell.nix
-{ pkgs ? import <nixpkgs> {} }:
-let
-  my-python-packages = ps: with ps; [
-      numpy
-    # other python packages
-  ];
-  my-python = pkgs.python311.withPackages my-python-packages;
-in my-python.env
