f61715744066dffdd89c69153c350d6fec8fe683
augustin.hubert <augustin.hubert@epita.fr>
2023-11-02 23:56:56 +0100

    im a genius

--- hough/hough.c
+++ hough/hough.c
@@ -5,8 +5,8 @@
 #include <math.h>
 #include <err.h>
 
-#define CloseAngle(t1, t2) (5 + abs(t1 - t2) % 180) < 10
-
+#define CloseAngle(t1, t2, threshold) (threshold + abs(t1 - t2) % 180) < (threshold<<1)
+#define toRad(x) x*M_PI/180.0
 typedef struct NodeLine{
 	struct NodeLine * next;
 	double rho;
@@ -14,10 +14,30 @@ typedef struct NodeLine{
 } NodeLine;
 
 typedef struct ListLine {
-	struct NodeLine* head;
+	NodeLine* head;
+	int size;
 } ListLine;
 
-void MyDrawLine(SDL_Surface* s, int x1, int y1, int x2, int y2)
+typedef struct Point {
+	int x;
+	int y;
+} Point;
+
+
+typedef struct NodeQuadrilateral{
+	struct NodeQuadrilateral * next;
+	Point p1,p2,p3,p4;
+} NodeQuadrilateral;
+
+
+typedef struct ListQuad {
+	NodeQuadrilateral* head;
+} ListQuad;
+
+
+
+
+void MyDrawLine(SDL_Surface* s, int x1, int y1, int x2, int y2, int r, int g, int b)
 {
 	//printf("trying to draw : w=%i h=%i ||x0 : %i  y0 : %i  x1 : %i  y1 : %i\n", s->w,s->h,x1,y1,x2,y2);
 
@@ -35,7 +55,7 @@ void MyDrawLine(SDL_Surface* s, int x1, int y1, int x2, int y2)
     {
         if (0 <= x1 && x1 < s->w && 0 <= y1 && y1 < s->h)
         {
-            pixels[x1 + y1 * s->w] = SDL_MapRGB(s->format, 255, 255, 255);
+            pixels[x1 + y1 * s->w] = SDL_MapRGB(s->format, r, g, b);
         }
 
         e2 = 2 * err;
@@ -79,7 +99,14 @@ void spread_arr(int size, double min, double max, double step, double* array)
     }
 }
 
-SDL_Surface* HoughLine(SDL_Surface* img, ListLine* line_list)
+void Preppend(ListLine* l, NodeLine* nd){
+	nd->next = l->head;
+	l->head = nd;
+	l->size++;
+}
+
+
+ListLine* HoughLine(SDL_Surface* img)
 {
 	int width = img->w;
 	int height = img->h;
@@ -89,10 +116,10 @@ SDL_Surface* HoughLine(SDL_Surface* img, ListLine* line_list)
 	// Cache some resuable values
 	double cos_t[180];
 	double sin_t[180];
-	for (int i = -90; i < 90; i++)
+	for (int i = -45; i < 135; i++)
 	{
-		cos_t[i + 90] = cos(i * M_PI / 180.0);
-		sin_t[i + 90] = sin(i * M_PI / 180.0);
+		cos_t[i + 45] = cos(toRad(i));
+		sin_t[i + 45] = sin(toRad(i));
 	}
 		
 	// Hough accumulator array of theta vs rho
@@ -115,9 +142,6 @@ SDL_Surface* HoughLine(SDL_Surface* img, ListLine* line_list)
 		}
 	}
 
-
-	SDL_Surface* newS = SDL_CreateRGBSurface(0,width, height, 32,0,0,0,0);
-
 	unsigned int maxVal = 0;
 
 	for (int i = 0; i < diag_len * 2; i++)
@@ -126,9 +150,6 @@ SDL_Surface* HoughLine(SDL_Surface* img, ListLine* line_list)
 		if (accumulator[i][j] > maxVal)
 			maxVal = accumulator[i][j];
 	}
-
-	
-	SDL_LockSurface(newS);
 
 	Visualize_Acc(accumulator, diag_len * 2, maxVal);
 
@@ -144,6 +165,10 @@ SDL_Surface* HoughLine(SDL_Surface* img, ListLine* line_list)
 	
     spread_arr(rho_num + 1, rho_min, rho_max, rho_step, rhos);
 
+	ListLine* list = malloc(sizeof(ListLine)); 
+	list->head = NULL;
+	list->size =0;
+	
 	for (int t = 0; t < 180; t += step)
     for (int r = 0; r <= diag_len*2; r += step)
 	{
@@ -168,38 +193,16 @@ SDL_Surface* HoughLine(SDL_Surface* img, ListLine* line_list)
 				maxTheta = y;
 			}
 		}
-
 		if (val < line_threshold)
 			continue;
 
-		//drawing the lines on a surface
-		double rho = rhos[maxRho];
-		//double rho = maxRho - diag_len/2;
-
 		NodeLine *nd = malloc(sizeof(NodeLine));
 		
-		nd->next = line_list->head;
-		
-		nd->rho = rho;
-		nd->theta = maxTheta;
-		line_list->head = nd;
-
-		double c = cos_t[maxTheta];
-		double s = sin_t[maxTheta];
-
-		int x0 = rho * c;
-		int y0 = rho * s;
-		
-		int x1 = x0 - diag_len * s;
-		int y1 = y0 + diag_len * c;
-
-		int x2 = x0 + diag_len * s;
-		int y2 = y0 - diag_len * c;
-
-		MyDrawLine(newS, x1, y1, x2, y2);
+		nd->rho = rhos[maxRho];
+		nd->theta = maxTheta-45%180;
+		Preppend(list, nd);
 	}
-
-	return newS;
+	return list;
 }
 
 SDL_Surface* load_image(const char* path)
@@ -238,28 +241,26 @@ double DoubleAbs(double x)
 //Removes all lines which are too close to each other
 void Prune(ListLine* l)
 {
-    if (!(l->head->next))
+	NodeLine* curr;
+    if (!(curr = l->head))
 	{
         return;
 	}
-
-	NodeLine* curr = l->head;
 
     while (curr->next)
     {
         NodeLine* curr2 = curr;
 		while (curr2->next)
 		{
-			if (CloseAngle(curr->theta, curr2->next->theta) &&
-				DoubleAbs(curr2->next->rho / curr->rho) < 1.1 &&
-				DoubleAbs(curr2->next->rho / curr->rho) > 0.9)
+			if (CloseAngle(curr->theta, curr2->next->theta,5) &&
+				DoubleAbs( DoubleAbs(curr2->next->rho / curr->rho)-1) < 0.05)
 			{
 				NodeLine* freeing = curr2->next;
-
 				//TODO average (beware, rhos can be < 0) and thetas can be equivalent
-
+				curr->rho = (curr->rho+freeing->rho)/2;
+				curr->theta = (curr->theta+freeing->theta)/2;
 				curr2->next = curr2->next->next;
-
+				l->size--;
 				free(freeing);
 			}
 			else
@@ -270,202 +271,244 @@ void Prune(ListLine* l)
     }
 }
 
-SDL_Surface* ListToSurface(ListLine* list, int width, int height)
+SDL_Surface* ListToSurface(SDL_Surface* s,ListLine* list)
 {
-	SDL_Surface* newS = SDL_CreateRGBSurface(0, width, height, 32,0,0,0,0);
-	SDL_LockSurface(newS);
 	NodeLine* curr = list->head;
+	int diag_len = sqrt(s->w * s->w + s->h * s->h);
 
-	int diag_len = sqrt(width * width + height * height);
-
-	double cos_t[180];
-	double sin_t[180];
-	for (int i = -90; i < 90; i++)
-	{
-		cos_t[i + 90] = cos(i * M_PI / 180.0);
-		sin_t[i + 90] = sin(i * M_PI / 180.0);
-	}
-
+	SDL_LockSurface(s);
 	while (curr)
 	{
-		double c = cos_t[curr->theta];
-		double s = sin_t[curr->theta];
+		double co = DoubleAbs(cos(toRad(curr->theta)));
+		double si = DoubleAbs(sin(toRad(curr->theta)));
 
-		int x0 = curr->rho * c;
-		int y0 = curr->rho * s;
+		int x0 = curr->rho * co;
+		int y0 = curr->rho * si;
 		
-		int x1 = x0 - diag_len * s;
-		int y1 = y0 + diag_len * c;
+		int x1 = x0 - diag_len * si;
+		int y1 = y0 + diag_len * co;
 
-		int x2 = x0 + diag_len * s;
-		int y2 = y0 - diag_len * c;
+		int x2 = x0 + diag_len * si;
+		int y2 = y0 - diag_len * co;
 
-		MyDrawLine(newS, x1, y1, x2, y2);
+		MyDrawLine(s, x1, y1, x2, y2,0,255,255);
 
 		curr = curr->next;
 	}
-
-	SDL_UnlockSurface(newS);
-	return newS;
+	SDL_UnlockSurface(s);
+	return s;
 }
 
-//Prunes the list till there are only 4 edges left
-void FindEdges(ListLine* list, NodeLine* l[4], NodeLine* l2[4])
+void DrawSquare(SDL_Surface* s, NodeQuadrilateral *q)
 {
-	//Have 2 lists of 4 nodes, the 4 nodes will be the 4 edges
-	// (4 main horizontal and vertical lines)
-	// Works by checking every pair of edges
-	// then checking the list which elements share a multiple of that distance
+	SDL_LockSurface(s);
+    MyDrawLine(s, q->p1.x , q->p1.y , q->p2.x , q->p2.y ,255,0,0);
+    MyDrawLine(s, q->p2.x , q->p2.y , q->p3.x , q->p3.y ,255,0,0);
+    MyDrawLine(s, q->p3.x , q->p3.y , q->p4.x , q->p4.y ,255,0,0);
+    MyDrawLine(s, q->p4.x , q->p4.y , q->p1.x , q->p1.y ,255,0,0);
+	SDL_UnlockSurface(s);
+}
 
-	int lorl2 = 0;
+int FindIntersection(NodeLine* l1 ,NodeLine* l2, int w, int h,int *x, int *y ){
+	//lines are not parallel
+	float cost1=cosf(toRad(l1->theta));
+    float cost2=cosf(toRad(l2->theta));
+    float sint1=sinf(toRad(l1->theta));
+    float sint2=sinf(toRad(l2->theta));
+    float a=cost1*sint2-sint1*cost2;  
+    *x = (int)((sint2*l1->rho - sint1*l2->rho)/a);
+	*y = (int)((cost1*l2->rho - cost2*l1->rho)/a);
+	//check if the intersection is not out of bound 
+	return *x >0 && *y >0 && *x<w && *y<h;
+}
+
+double Min4(double a,double b,double c,double d){
+	return (a < b ? (a < c ? (a < d ? a : d) : (c < d ? c : d)) :
+					(b < c ? (b < d ? b : d) : (c < d ? c : d)));
+}
+
+double Max4(double a,double b,double c,double d){
+	return (a > b ? (a > c ? (a > d ? a : d) : (c > d ? c : d)) :
+					(b > c ? (b > d ? b : d) : (c > d ? c : d)));
+}
+
+double Dist(Point p1, Point p2)
+{
+    double dX = p1.x - p2.x;
+    double dY = p1.y - p2.y;
+    return sqrt(dX*dX + dY*dY);
+}
+
+double Area(Point p1,Point p2,Point p3,Point p4)
+{
+    // area of a triangle = base * height / 2
+    double base = Dist(p1, p3);
+
+    double dist2to3 = Dist(p2, p3);
+    double height1 = sqrt(dist2to3 * dist2to3 - base * base / 4);
+    
+    double a = base * height1 / 2;//Area of the upper triangle (1,2,3)
+
+    double dist3to4 = Dist(p3, p4);
+    double height2 = sqrt(dist3to4 * dist3to4 - base * base / 4);
+
+    a += base * height2 / 2;//Area of the lower triangle (1,3,4)
+	return a;
+}
+
+double MinMaxRatio(Point p1,Point p2,Point p3,Point p4){
+	double l1 = Dist(p1,p2);
+	double l2 = Dist(p2,p3);
+	double l3 = Dist(p3,p4);
+	double l4 = Dist(p4,p1);
+	double mi = Min4(l1,l2,l3,l4);
+	double ma = Max4(l1,l2,l3,l4);
+	
+	return DoubleAbs(mi/ma-1);
+}
+
+NodeQuadrilateral* BestSquare(ListQuad *l){
+	if(!l->head) err(EXIT_FAILURE,"empty quad list\n");
+
+	NodeQuadrilateral* currQuad = l->head;
+	NodeQuadrilateral* bestSquare = l->head;
+	double bestRatio = 0;
+
+	while(currQuad){
+		Point p1 = currQuad->p1;
+    	Point p2 = currQuad->p2;
+    	Point p3 = currQuad->p3;
+    	Point p4 = currQuad->p4;
+		double l1 = Dist(p1,p2);
+		double l2 = Dist(p2,p3);
+		double l3 = Dist(p3,p4);
+		double l4 = Dist(p4,p1);
+		double min = Min4(l1,l2,l3,l4);
+		double max = Max4(l1,l2,l3,l4);
+		min = min*min;
+		max = max*max;
+		double areaRatio  = (max - min)/max *min;
+		double selectionRatio =min * 3 - areaRatio * 10; 
+		if(selectionRatio> bestRatio){
+			bestRatio = selectionRatio;
+			bestSquare = currQuad;
+		}
+		currQuad = currQuad->next;
+	}
+	return bestSquare;
+}
+
+void printQuad(NodeQuadrilateral* q){
+	printf("	square pointer %p\n",q);
+	printf("	p1 x: %3i  y: %3i \n",q->p1.x,q->p1.y);
+	printf("	p2 x: %3i  y: %3i \n",q->p2.x,q->p2.y);
+	printf("	p3 x: %3i  y: %3i \n",q->p3.x,q->p3.y);
+	printf("	p4 x: %3i  y: %3i \n",q->p4.x,q->p4.y);
+}
+
+ListQuad* FindSquares(ListLine* l,int width, int height){
+	
+	Point intersection[l->size][l->size];
+	
+	NodeLine *curr = l->head;
 	int index = 0;
-	int index2 = 0;
 
-	NodeLine* curr = list->head;
-	while (curr)
-	{
-		if (lorl2)
-			l2[0] = curr;
-		else
-			l[0] = curr;
-		NodeLine* curr2 = curr->next;
-		while (curr2)
-		{
-			if (CloseAngle(curr2->theta, curr->theta) || curr2->rho == curr->rho)
-			{
-				curr2 = curr2->next;
-				continue;
+	Point p;
+	
+	while(curr){
+		int angle = curr->theta +90 %180;
+		NodeLine *innerCurr = l->head;
+		int innerIndex =0;
+		while(innerCurr){
+			int e =CloseAngle(angle,innerCurr->theta,30); 
+			if( e && FindIntersection(curr,innerCurr,width,height,&(p.x),&(p.y))){
+				intersection[index][innerIndex] = p; 
 			}
-			if (lorl2)
-				l2[1] = curr2;
-			else
-				l[1] = curr2;
-			NodeLine* curr3 = curr2->next;
-			if (index2 < 4)
-			{
-				if (lorl2)
-					index2 = 2;
-				else
-					index = 2;
+			else {
+				intersection[index][innerIndex] = (Point){x:-1,y:-1};
 			}
-
-			while (curr3)
-			{
-				if (CloseAngle(curr3->theta, curr->theta) || curr3->rho == curr->rho)
-				{
-					curr3 = curr3->next;
-					continue;
-				}
-				if ((((int)(curr->rho / 100 + curr3->rho - curr->rho)) %
-					((int)(curr2->rho - curr->rho)))
-					< curr->rho / 100)
-				{
-					if (lorl2)
-					{
-						if (index2 < 4)
-						{
-							l2[index2] = curr3;
-						}
-						index2 += 1;
-					}
-					else
-					{
-						if (index < 4)
-						{
-							l[index] = curr3;
-						}
-						index += 1;
-					}
-				}
-				curr3 = curr3->next;
-			}
-			if (!lorl2 && index == 4)
-				lorl2 = 1;//Found 4 horizontal/vertical, can find the other now
-			if (lorl2 && index2 == 4)
-				return;
-			curr2 = curr2->next;
+			innerIndex++;
+			innerCurr= innerCurr->next;
 		}
 		curr = curr->next;
+		index++;
 	}
+
+	printf("----");
+	for (int j = 0; j < l->size; j++)
+		printf("[%2i]        ",j);
+	printf("\n");			
+	for (int i =0 ; i < l->size; i++)
+	{
+		printf("[%2i]",i);
+		for (int j = 0; j < l->size; j++)
+		{
+			printf("x:%3i y:%3i ",intersection[i][j].x,intersection[i][j].y);
+		}
+		printf("\n");
+	}
+
+	ListQuad *lquad = malloc(sizeof(ListQuad));
+	lquad->head = NULL;
+	for(int i1 = 0; i1<l->size;i1++)
+		for(int i2 = 0; i2<l->size;i2++)
+			if(intersection[i1][i2].x !=-1)
+				for (int i3 = 0; i3 < l->size; i3++)
+					if(i3!=i1 && intersection[i2][i3].x !=-1)
+						for (int i4 = 0; i4 < l->size; i4++)
+							if(i4!=i2 && intersection[i3][i4].x !=-1)
+								if(intersection[i4][i1].x != -1){
+									NodeQuadrilateral *nd = malloc(sizeof(NodeQuadrilateral));
+									nd->next = lquad->head;
+									nd->p1 = intersection[i1][i2];
+									nd->p2 = intersection[i2][i3];
+									nd->p3 = intersection[i3][i4];
+									nd->p4 = intersection[i4][i1];
+									lquad->head = nd;
+								}
+	
+	return lquad;
 }
+
+void FreeList(ListLine *l)
+{
+    NodeLine* curr = l->head;
+
+    while (curr)
+    {
+        NodeLine* prev = curr;
+        curr = curr->next;
+        free(prev);
+    }
+    
+    free(l);
+}
+
 
 int main(int argc, char** argv)
 {
 	if (argc != 3)
 		errx(1,"first param : path_in\nsecond param : path_out\n");
 	SDL_Surface* input = load_image(argv[1]);
-	ListLine list = { head : NULL };
+	
 
-	SDL_Surface* output = HoughLine(input, &list);
-	printList(&list);
-	Prune(&list);
-	printList(&list);
+	ListLine* list = HoughLine(input);
+	Prune(list);
+	printList(list);
+	IMG_SavePNG(ListToSurface(input,list), "lines.png");
+	printf("lines done\n");
+	ListQuad* lquad =  FindSquares(list,input->w,input->h);
+	NodeQuadrilateral * grid = BestSquare(lquad);
+	printf("square found :\n");
+	printQuad(grid);
+	
 
-	IMG_SavePNG(ListToSurface(&list, output->w, output->h), argv[2]);
+	DrawSquare(input,grid);
+	printf("square drown\n");
+	IMG_SavePNG(input, argv[2]);
+
 	SDL_FreeSurface(input);
-	SDL_FreeSurface(output);
-
-	NodeLine* l[4];
-	NodeLine* l2[4];
-
-	FindEdges(&list, l, l2);
-
-	ListLine li = { head : NULL};
-	NodeLine* nd;
-
-	nd = malloc(sizeof(NodeLine));
-	nd->next = li.head;
-	nd->rho = l[0]->rho;
-	nd->theta = l[0]->theta;
-	li.head = nd;
-
-	nd = malloc(sizeof(NodeLine));
-	nd->next = li.head;
-	nd->rho = l[1]->rho;
-	nd->theta = l[1]->theta;
-	li.head = nd;
-
-	nd = malloc(sizeof(NodeLine));
-	nd->next = li.head;
-	nd->rho = l[2]->rho;
-	nd->theta = l[2]->theta;
-	li.head = nd;
-	
-	nd = malloc(sizeof(NodeLine));
-	nd->next = li.head;
-	nd->rho = l[3]->rho;
-	nd->theta = l[3]->theta;
-	li.head = nd;
-	
-	nd = malloc(sizeof(NodeLine));
-	nd->next = li.head;
-	nd->rho = l2[0]->rho;
-	nd->theta = l2[0]->theta;
-	li.head = nd;
-
-	nd = malloc(sizeof(NodeLine));
-	nd->next = li.head;
-	nd->rho = l2[1]->rho;
-	nd->theta = l2[1]->theta;
-	li.head = nd;
-
-	nd = malloc(sizeof(NodeLine));
-	nd->next = li.head;
-	nd->rho = l2[2]->rho;
-	nd->theta = l2[2]->theta;
-	li.head = nd;
-	
-	nd = malloc(sizeof(NodeLine));
-	nd->next = li.head;
-	nd->rho = l2[3]->rho;
-	nd->theta = l2[3]->theta;
-	li.head = nd;
-	
-	
-	IMG_SavePNG(ListToSurface(&li, output->w, output->h), "edges.png");
-
+	FreeList(list);
 	printf("\n\nAfter prune: ");
-	printList(&li);
-	
 	return 0;
 }
