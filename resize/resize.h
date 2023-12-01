816fd3c9edf8d862a72b9d77028d1dbceac975df
Taliayaya <ilanvinord@gmail.com>
2023-12-01 21:17:15 +0100

    downscale resize

--- resize/resize.h
+++ resize/resize.h
@@ -1,6 +1,19 @@
 #include <err.h>
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL.h>
+# include <math.h>
+
+/**
+  * A better version of the resizes. It better handles small images resizing
+  * and avoid pixel lose by making the average of neigbours.
+  * Also called Box sampling resizing
+  *
+  * surface: the surface to resize
+  * new_size: the new width and height of the surface
+  *
+  * Return a new allocated resized surface
+  */
+SDL_Surface* downscale_resize(SDL_Surface *surface, int new_size);
 
 SDL_Surface* Resize(SDL_Surface* surface, int newSize);
 
