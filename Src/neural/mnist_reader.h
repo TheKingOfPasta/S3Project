cbfc5e6b0db7ef7386c60ffe7bea54804343917b
Aurelien Marques <aurelien.marques@epita.fr>
2023-12-08 23:10:31 +0100

    no share code here

--- Src/neural/mnist_reader.h
+++ Src/neural/mnist_reader.h
@@ -10,15 +10,10 @@
 #define TEST_IMAGE "database/t10k-images.idx3-ubyte"
 #define TEST_LABEL "database/t10k-labels.idx1-ubyte"
 
-#define SIZE 784 // 28*28
-#define NUM_TRAIN 60000
-#define NUM_TEST 10000
 #define LEN_INFO_IMAGE 4
 #define LEN_INFO_LABEL 2
 
 #define MAX_IMAGESIZE 1280
-#define MAX_BRIGHTNESS 255
-#define MAX_FILENAME 256
 #define MAX_NUM_OF_IMAGES 1
 
 unsigned char image[MAX_NUM_OF_IMAGES][MAX_IMAGESIZE][MAX_IMAGESIZE];
@@ -27,38 +22,17 @@ int width[MAX_NUM_OF_IMAGES], height[MAX_NUM_OF_IMAGES];
 int info_image[LEN_INFO_IMAGE];
 int info_label[LEN_INFO_LABEL];
 
-unsigned char train_image_char[NUM_TRAIN][SIZE];
-unsigned char test_image_char[NUM_TEST][SIZE];
-unsigned char train_label_char[NUM_TRAIN][1];
-unsigned char test_label_char[NUM_TEST][1];
+unsigned char train_image_char[60000][784];
+unsigned char test_image_char[10000][784];
+unsigned char train_label_char[60000][1];
+unsigned char test_label_char[10000][1];
 
-double train_image[NUM_TRAIN][SIZE];
-double test_image[NUM_TEST][SIZE];
-int  train_label[NUM_TRAIN];
-int test_label[NUM_TEST];
+double train_image[60000][784];
+double test_image[10000][784];
+int  train_label[60000];
+int test_label[10000];
 
-
-
-
-
-
-/*
- * This entire file was found on github
- * https://github.com/takafumihoriuchi/MNIST_for_C
- * the call to load_mnist() fills train_image / test_image
- * with 60000 / 10000 784 long arrays representing images
- * it also fills train_label / test_label with
- * 60000 / 10000 ints representing the number that is given in the img from
- * train_image / test_image
-*/
-
-
-
-
-
-
-
-void FlipLong(unsigned char * ptr)
+void Flip(unsigned char * ptr)
 {
     register unsigned char val;
 
@@ -74,8 +48,7 @@ void FlipLong(unsigned char * ptr)
     *(ptr+1) = val;
 }
 
-
-void read_mnist_char(
+void read_chars(
         char *file_path,
         int num_data,
         int len_info,
@@ -83,7 +56,8 @@ void read_mnist_char(
         unsigned char data_char[][arr_n],
         int info_arr[])
 {
-    int i, fd;
+    int i;
+    int fd;
     unsigned char *ptr;
 
     if ((fd = open(file_path, O_RDONLY)) == -1) {
@@ -93,50 +67,48 @@ void read_mnist_char(
 
     read(fd, info_arr, len_info * sizeof(int));
 
-    // read-in information about size of data
-    for (i=0; i<len_info; i++) {
+    for (i = 0; i < len_info; i++) {
         ptr = (unsigned char *)(info_arr + i);
-        FlipLong(ptr);
+        Flip(ptr);
         ptr = ptr + sizeof(int);
     }
 
-    // read-in mnist numbers (pixels|labels)
-    for (i=0; i<num_data; i++) {
+    for (i = 0; i < num_data; i++) {
         read(fd, data_char[i], arr_n * sizeof(unsigned char));
     }
 
     close(fd);
 }
 
-
-void image_char2double(int num_data, unsigned char data_image_char[][SIZE], double data_image[][SIZE])
+void img_ctod(int num_data, unsigned char data_image_char[][784], double data_image[][784])
 {
-    int i, j;
-    for (i=0; i<num_data; i++)
-        for (j=0; j<SIZE; j++)
+    int i;
+    int j;
+
+    for (i = 0; i < num_data; i++)
+        for (j = 0; j < 784; j++)
             data_image[i][j]  = (double)data_image_char[i][j] / 255.0;
 }
 
-
-void label_char2int(int num_data, unsigned char data_label_char[][1], int data_label[])
+void label_ctoi(int num_data, unsigned char data_label_char[][1], int data_label[])
 {
     int i;
-    for (i=0; i<num_data; i++)
+
+    for (i = 0; i < num_data; i++)
         data_label[i]  = (int)data_label_char[i][0];
 }
 
-
 void load_mnist()
 {
-    read_mnist_char(TRAIN_IMAGE, NUM_TRAIN, LEN_INFO_IMAGE, SIZE, train_image_char, info_image);
-    image_char2double(NUM_TRAIN, train_image_char, train_image);
+    read_chars(TRAIN_IMAGE, 60000, LEN_INFO_IMAGE, 784, train_image_char, info_image);
+    img_ctod(60000, train_image_char, train_image);
 
-    read_mnist_char(TEST_IMAGE, NUM_TEST, LEN_INFO_IMAGE, SIZE, test_image_char, info_image);
-    image_char2double(NUM_TEST, test_image_char, test_image);
+    read_chars(TEST_IMAGE, 10000, LEN_INFO_IMAGE, 784, test_image_char, info_image);
+    img_ctod(10000, test_image_char, test_image);
 
-    read_mnist_char(TRAIN_LABEL, NUM_TRAIN, LEN_INFO_LABEL, 1, train_label_char, info_label);
-    label_char2int(NUM_TRAIN, train_label_char, train_label);
+    read_chars(TRAIN_LABEL, 60000, LEN_INFO_LABEL, 1, train_label_char, info_label);
+    label_ctoi(60000, train_label_char, train_label);
 
-    read_mnist_char(TEST_LABEL, NUM_TEST, LEN_INFO_LABEL, 1, test_label_char, info_label);
-    label_char2int(NUM_TEST, test_label_char, test_label);
+    read_chars(TEST_LABEL, 10000, LEN_INFO_LABEL, 1, test_label_char, info_label);
+    label_ctoi(10000, test_label_char, test_label);
 }
